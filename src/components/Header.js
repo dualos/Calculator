952341af4dbdca3498c74742961c2c2d2b0d52c9
Jacob Tuininga <dualos13tuininga@gmail.com>
2020-09-23 15:16:27 -0700

    commit on 'master'

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="App">
+            <header className="App-header">Calculator</header>
+        </div>
+    );
+}
+
+export default Header;
